c824a5075eba178098a7acb38239247af9cc06e1
suto0507 <suto0507@gmail.com>
2021-06-15 18:21:15 +0900

    create SpoutScecne and make 0~1->Color(blur~red)

---
+++ Assets/ColorChangeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeScript : MonoBehaviour
+{
+    [SerializeField]
+    private float unazuki;
+    public float Unazuki
+    {
+        get
+        {
+            return unazuki;
+        }
+        set
+        {
+            unazuki = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        unazuki = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color newColor = UnazukiColor();
+        GetComponent<Renderer>().material.color = newColor;
+    }
+
+    Color UnazukiColor()
+    {
+        float r = unazuki * 0.7f + 0.1f;
+        float g = 0.5f - Mathf.Abs(unazuki - 0.5f)*1.0f + 0.2f - unazuki*0.1f;
+        float b = (1 - unazuki) * 0.4f + 0.1f;
+        return new Color(r, g, b, 1);
+    }
+}
