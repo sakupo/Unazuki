eb013ba6e99b2180a5a0ab198e2ab6ba6d31bb77
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-06-20 01:19:22 +0900

    zoom urlを入力するための入力欄とボタンを設置

---
+++ Assets/Scripts/InputZoomUrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputZoomUrl : MonoBehaviour {
+
+    //オブジェクトと結びつける
+    public InputField inputZoomUrlField;
+    public Text inputZoomUrltext;
+
+    void Start() {
+        //Componentを扱えるようにする
+        inputZoomUrlField = inputZoomUrlField.GetComponent<InputField> ();
+        inputZoomUrltext = inputZoomUrltext.GetComponent<Text> ();
+    }
+
+    public void InputText() {
+        //テキストにinputZoomUrlFieldの内容を反映
+        inputZoomUrltext.text = inputZoomUrlField.text; 
+    }
+    
+    /// ボタンをクリックした時の処理
+    public void OnClick() {
+        Debug.Log(inputZoomUrlField.text);
+        inputZoomUrlField.text = "";
+    }
+
+}
