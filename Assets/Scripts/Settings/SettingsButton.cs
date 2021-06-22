467e4aea494ef84d178f13a290663003f3fb32b6
hshs17 <gfsyuysu0819@gmsil.com>
2021-06-22 17:53:05 +0900

    設定画面にボタンを追加

---
+++ Assets/Scripts/Settings/SettingsButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsButton : MonoBehaviour
+{ 
+    private Button settingsButton;
+    private GameObject inputAPI;
+    private bool show = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        settingsButton = GetComponent<Button>();
+        inputAPI = GameObject.FindGameObjectWithTag("InputAPI");
+        inputAPI.SetActive(show);
+        settingsButton.onClick.AddListener(OnClickSettingsButton);
+    }
+
+    public void OnClickSettingsButton() 
+    {
+        show = !show;
+        inputAPI.SetActive(show);
+        Debug.Log("clicked " + show);
+    }
+
+}
+
