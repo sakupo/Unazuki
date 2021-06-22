1ba363e52508e27a1506296c54957dc72a29933b
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-06-22 15:12:34 +0900

    RoomNameの入力欄を追加

--- Assets/Scripts/InputZoomUrl.cs
+++ Assets/Scripts/InputZoomUrl.cs
@@ -6,23 +6,31 @@ using UnityEngine.UI;
 public class InputZoomUrl : MonoBehaviour {
 
     //オブジェクトと結びつける
+    public InputField inputRoomNameField;
+    public Text inputRoomNametext;
     public InputField inputZoomUrlField;
     public Text inputZoomUrltext;
 
     void Start() {
         //Componentを扱えるようにする
+        inputRoomNameField = inputRoomNameField.GetComponent<InputField> ();
+        inputRoomNametext = inputRoomNametext.GetComponent<Text> ();
         inputZoomUrlField = inputZoomUrlField.GetComponent<InputField> ();
         inputZoomUrltext = inputZoomUrltext.GetComponent<Text> ();
     }
 
     public void InputText() {
         //テキストにinputZoomUrlFieldの内容を反映
-        inputZoomUrltext.text = inputZoomUrlField.text; 
+        inputRoomNametext.text = inputRoomNameField.text;
+        inputZoomUrltext.text = inputZoomUrlField.text;
     }
     
     /// ボタンをクリックした時の処理
     public void OnClick() {
+        Debug.Log(inputRoomNameField.text);
         Debug.Log(inputZoomUrlField.text);
+
+        inputRoomNameField.text = "";
         inputZoomUrlField.text = "";
     }
 
