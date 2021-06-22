4d80860367c199e8e7c6f8dcea20785675e7a6e7
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-06-22 15:59:16 +0900

    ディレクトリ構造を変更

---
+++ Assets/Scripts/InputZoomUrl/InputZoomUrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InputZoomUrl
+{
+
+    public class InputZoomUrl : MonoBehaviour
+    {
+
+        //オブジェクトと結びつける
+        public InputField inputRoomNameField;
+        public Text inputRoomNametext;
+        public InputField inputZoomUrlField;
+        public Text inputZoomUrltext;
+
+        void Start()
+        {
+            //Componentを扱えるようにする
+            inputRoomNameField = inputRoomNameField.GetComponent<InputField>();
+            inputRoomNametext = inputRoomNametext.GetComponent<Text>();
+            inputZoomUrlField = inputZoomUrlField.GetComponent<InputField>();
+            inputZoomUrltext = inputZoomUrltext.GetComponent<Text>();
+        }
+
+        public void InputText()
+        {
+            //テキストにinputZoomUrlFieldの内容を反映
+            inputRoomNametext.text = inputRoomNameField.text;
+            inputZoomUrltext.text = inputZoomUrlField.text;
+        }
+
+        /// ボタンをクリックした時の処理
+        public void OnClick()
+        {
+            Debug.Log(inputRoomNameField.text);
+            Debug.Log(inputZoomUrlField.text);
+
+            inputRoomNameField.text = "";
+            inputZoomUrlField.text = "";
+        }
+    }
+
+}
