da32509025e8049519e71867e54c725b10404b80
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-06-29 14:32:59 +0900

    画面遷移用のボタンを複数表示

---
+++ Assets/Scripts/InputZoomUrl/DisplayRooms.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InputZoomUrl {
+	public class DisplayRooms : MonoBehaviour
+	{
+    	public GameObject canvas;
+	    private Vector3 centerToLeftTop; // canvasの中心から左上の座標までのベクトル
+    	public Button baseRoomButton; // clone元となるButtonオブジェクト
+	    private float buttonWidth;
+    	private float buttonHeight;
+	    private ZoomUrlsData zoomUrlsData;  // room名とurlのペアを全て保存するデータ構造
+    
+    
+	    void Start()
+    	{
+        	// 座標変換用に、canvasの中心から左上の座標までのベクトルを計算
+	        centerToLeftTop = new Vector3((-1) * canvas.GetComponent<RectTransform>().sizeDelta.x / 2,
+    	        canvas.GetComponent<RectTransform>().sizeDelta.y / 2, 0);
+        
+        	buttonWidth = baseRoomButton.GetComponent<RectTransform>().sizeDelta.x;
+ 	        buttonHeight = baseRoomButton.GetComponent<RectTransform>().sizeDelta.y;
+
+    	    zoomUrlsData = new ZoomUrlsData();
+
+
+	        DisplayAllRoomButtons();
+    	}
+
+		// zoomUrlsDataに保存されている全てのデータを描画する
+	    private void DisplayAllRoomButtons()
+    	{
+			for(int index = 0; index < zoomUrlsData.Count(); index++){
+				DisplayRoomButton(index);
+			}
+    	}
+
+		// ZoomUrlsDataのn番目のデータを、ボタンとしてcanvasに描画する
+    	private void DisplayRoomButton(int n)
+	    {
+			Tuple<string, string> zoomUrlData = zoomUrlsData.Get(n);
+			string roomName = zoomUrlData.Item1;
+			string zoomUrl = zoomUrlData.Item2;
+
+    	    Button clonedButton = Instantiate(baseRoomButton, canvas.transform);
+        	Vector3 position = ConvertPosition(n);
+	        clonedButton.transform.localPosition = position;
+
+        	foreach (Text text in clonedButton.GetComponentsInChildren<Text>())
+	        {
+    	        if (text.name == "RoomName")
+        	    {
+            	    text.text = roomName;
+	            }
+    	        else if (text.name == "ZoomUrl")
+        	    {
+            	    text.text = zoomUrl;
+            	}
+	        }
+
+	        // click時のcall back関数を登録
+    	    clonedButton.GetComponent<Button>().onClick.AddListener(() => ClickRoomButton(n));
+	    }
+
+	    // 上から何番目のButtonかを引数(n)で受け取る
+    	// canvasの中心基準で、buttonを配置すべき座標を計算し、Vector3オブジェクトとして返す
+	    private Vector3 ConvertPosition(int n)
+    	{
+        	return new Vector3(centerToLeftTop.x + buttonWidth/2,
+            	centerToLeftTop.y - n * buttonHeight - buttonHeight/2, 0);
+	    }
+
+    	// roomのボタンが押されたときに呼ばれる関数
+	    void ClickRoomButton(int n)
+    	{
+        	// Debug.Log(n + "-th room button is clicked!!!");
+	        Tuple<string, string> zoomUrlData = zoomUrlsData.Get(n);
+    	    Debug.Log("room name:'" + zoomUrlData.Item1 + "'(url:'" + zoomUrlData.Item2 + "') is clicked!!!");
+	    }
+	}
+}
---
+++ Assets/Scripts/InputZoomUrl/ZoomUrlsData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InputZoomUrl
+{
+    // room名とurlのペアを全て保存するデータ構造
+    public class ZoomUrlsData
+    {
+        private List<Tuple<string, string>> zoomUrlsData;
+
+        public ZoomUrlsData()
+        {
+            zoomUrlsData = new List<Tuple<string, string>>()
+            {
+                new Tuple<string, string>("aaa", "www.aaa"),
+                new Tuple<string, string>("bbb", "www.bbb"),
+                new Tuple<string, string>("ccc", "www.ccc")
+            };
+        }
+
+        // List中のn番目のデータ(room名とurlのタプル)を返す
+        public Tuple<string, string> Get(int n)
+        {
+            return zoomUrlsData[n];
+        }
+
+        // 新しいデータを追加
+        public void Add(string roomName, string zoomUrl)
+        {
+            zoomUrlsData.Add(new Tuple<string, string>(roomName, zoomUrl));
+        }
+
+        // データサイズを取得
+        public int Count()
+        {
+            return zoomUrlsData.Count;
+        }
+    }
+}
