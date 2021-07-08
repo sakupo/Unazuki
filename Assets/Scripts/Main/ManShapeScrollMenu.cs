4e3cbdcd29b532321c9533983c4a8c686e583338
sakupo <sakuposaku@icloud.com>
2021-07-08 12:50:57 +0900

    真ん中の形(manShape)を選択するMenuで使うprefabとscriptを追加

---
+++ Assets/Scripts/Main/ManShapeScrollMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Main
+{
+  public class ManShapeScrollMenu : MonoBehaviour
+  {
+    [SerializeField] private List<Mesh> shapeMeshes = new List<Mesh>();
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject menuItemPrefab;
+
+    void Start()
+    {
+      CreateMenu();
+    }
+
+    private void CreateMenu()
+    {
+      foreach (var mesh in shapeMeshes)
+      {
+        // menuItemの生成
+        var itemObj = Instantiate(menuItemPrefab, menu.transform, false);
+        var shapeMeshFilter = itemObj.GetComponentInChildren<MeshFilter>();
+        shapeMeshFilter.mesh = mesh;
+      }
+    }
+  }
+}
