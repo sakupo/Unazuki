f1195f9ab0ec6aaf8e7cafc7902cde62b91e375c
sakupo <sakuposaku@icloud.com>
2021-07-09 00:20:23 +0900

    うなずきバーが無効の時に動かせないようにした

--- Assets/Scripts/Main/UnazukiBarStateButton.cs
+++ Assets/Scripts/Main/UnazukiBarStateButton.cs
@@ -8,16 +8,26 @@ namespace Main
 {
   public class UnazukiBarStateButton : ObservableToggleButton
   {
+    [SerializeField] Slider slider;
+
+    private void Start()
+    {
+      // 起動時にoff
+      TurnOff();
+    }
+
     protected override void TurnOn()
     {
       background.color = Color.white;
       label.text = "うなずきバー: <color=#ff0000>有効</color>";
+      slider.interactable = true;
     }
 
     protected override void TurnOff()
     {
       background.color = Color.gray;
       label.text = "うなずきバー: <color=#333333>無効</color>";
+      slider.interactable = false;
     }
   }
 }
