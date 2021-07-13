b24c017a7798306f4fd7b831d8f693fef2b3d7fd
sakupo <sakuposaku@icloud.com>
2021-07-14 01:15:31 +0900

    UICanvasとHomeCanvas: CanvasExが重複していたのをHomeCanvas: UICanvasに統一

--- Assets/Scripts/InputZoomUrl/HomeCanvas.cs
+++ Assets/Scripts/InputZoomUrl/HomeCanvas.cs
@@ -5,19 +5,18 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Utility;
 
-
-public class HomeCanvas : CanvasEx
+namespace InputZoomUrl
 {
+  public class HomeCanvas : UICanvas
+  {
     protected override void InitAtStart()
     {
-#if DEBUG
-        // 開発環境では以下のCanvasを隠す
-        List<CanvasEx> canvasExs = GetCanvasExsFromScene("MainScene", "SpoutScene", "UnazukiScene");
-        foreach (var canvasEx in canvasExs)
-        {
-            canvasEx.HideCanvas();
-        }
-#endif
+      // 以下のCanvasを隠す
+      List<CanvasEx> canvasExs = GetCanvasExsFromScene("MainScene");
+      foreach (var canvasEx in canvasExs)
+      {
+        canvasEx.HideCanvas();
+      }
     }
-    
+  }
 }
