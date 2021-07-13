03dcf71e32d0f2055a0793f1ab49d90f7722ad7a
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-07-13 17:02:45 +0900

    HomeSceneからMainSceneの遷移を表示非表示で行う

---
+++ Assets/Scripts/InputZoomUrl/HomeCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Utility;
+
+
+public class HomeCanvas : CanvasEx
+{
+    protected override void InitAtStart()
+    {
+#if DEBUG
+        // 開発環境では以下のCanvasを隠す
+        List<CanvasEx> canvasExs = GetCanvasExsFromScene("MainScene", "SpoutScene", "UnazukiScene");
+        foreach (var canvasEx in canvasExs)
+        {
+            canvasEx.HideCanvas();
+        }
+#endif
+    }
+    
+}
