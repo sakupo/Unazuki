cb244067f026a43ff279b1fe4548b108dff6c09e
hshs17 <gfsyuysu0819@gmsil.com>
2021-07-14 14:04:18 +0900

    値の保存バグ修正

--- Assets/Scripts/Settings/SelectCamera.cs
+++ Assets/Scripts/Settings/SelectCamera.cs
@@ -18,14 +18,29 @@ namespace Settings
             {
                 selectCamera.ClearOptions();
                 List<string> list = new List<string>();
-                for (int i = 0; i<WebCamTexture.devices.Length; i++)
+                for (int i=0; i<WebCamTexture.devices.Length; i++)
                 {
                     list.Add(WebCamTexture.devices[i].name);
                 }
                 selectCamera.AddOptions(list);
                 // デフォルト値
-                selectCamera.value = 0;
-                PlayerPrefs.SetString(WebCamera.PlayerPrefsKeyCamera, WebCamTexture.devices[0].name);
+				if (PlayerPrefs.HasKey(WebCamera.PlayerPrefsKeyCamera))
+				{
+					string camera  = PlayerPrefs.GetString(WebCamera.PlayerPrefsKeyCamera);
+					int camera_i;
+
+					for (camera_i=0; camera_i<WebCamTexture.devices.Length; camera_i++)
+					{
+						if (camera.Equals(WebCamTexture.devices[camera_i].name)) break;
+					}
+					selectCamera.value = camera_i;
+				} 
+				else 
+				{ 
+					selectCamera.value = 0;
+                	PlayerPrefs.SetString(WebCamera.PlayerPrefsKeyCamera, WebCamTexture.devices[0].name);
+					PlayerPrefs.Save();
+				}
             }
         }
 
@@ -35,6 +50,7 @@ namespace Settings
             Debug.Log(selectCamera.value + " selected");
             string camera = WebCamTexture.devices[selectCamera.value].name;
             PlayerPrefs.SetString(WebCamera.PlayerPrefsKeyCamera, camera);
+			PlayerPrefs.Save();
             Debug.Log(camera + " selected");
         }
     }
