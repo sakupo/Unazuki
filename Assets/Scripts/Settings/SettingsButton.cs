71e6148333ad365d6aeaeb5756e5fe31b17a01df
sakupo <sakuposaku@icloud.com>
2021-07-16 18:04:58 +0900

    設定画面表示用のスクリプトを追加

--- Assets/Scripts/Settings/SettingsButton.cs
+++ Assets/Scripts/Settings/SettingsButton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Utility;
 
 namespace Settings
 {
@@ -11,12 +12,15 @@ namespace Settings
         private bool show = false;
 
         public GameObject parent;
+        private GameObject cameraObj;
+        [SerializeField] private GameObject bgCanvasObj;
 
 
         // Start is called before the first frame update
         void Start()
         {
             parent.SetActive(show);
+            bgCanvasObj.SetActive(show);
         }
 
         // ボタンクリック時の処理
@@ -24,6 +28,7 @@ namespace Settings
         {
             show = !show;
             parent.SetActive(show);
+            bgCanvasObj.SetActive(show);
             Debug.Log("clicked " + show);
         }
 
