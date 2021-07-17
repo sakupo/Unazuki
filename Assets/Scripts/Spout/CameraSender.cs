5abb30d5ae3d3f92085296568615d38b4c74f987
sakupo <sakuposaku@icloud.com>
2021-07-17 18:49:27 +0900

    MacOSにおいてSyphonを使用するように変更

--- Assets/Scripts/Spout/CameraSender.cs
+++ Assets/Scripts/Spout/CameraSender.cs
@@ -1,19 +1,30 @@
 using System;
 using Klak.Spout;
+using Klak.Syphon;
 using Main;
 using UnityEngine;
 using Utility;
 
 namespace Spout
 {
-  public class SpoutSenderEx : MonoBehaviour, IObserver<bool>
+  public class CameraSender : MonoBehaviour, IObserver<bool>
   {
     private SpoutSender spoutSender;
+    private SyphonServer syphonServer;
     private SpoutCamStateButton spoutCamStateButton;
 
     private void Start()
     {
       spoutSender = GetComponent<SpoutSender>();
+      syphonServer = GetComponent<SyphonServer>();
+#if UNITY_STANDALONE_WIN
+      spoutSender.enabled = true;
+      syphonServer.enabled = false;
+#endif
+#if UNITY_STANDALONE_OSX
+      spoutSender.enabled = false;
+      syphonServer.enabled = true;
+#endif
       spoutCamStateButton = CanvasEx.GetComponentInCanvasChildrenFromScene<SpoutCamStateButton>("MainScene");
       spoutCamStateButton.Subscribe(this);
     }
@@ -30,7 +41,12 @@ namespace Spout
 
     public void OnNext(bool isOn)
     {
+#if UNITY_STANDALONE_WIN
       spoutSender.enabled = isOn;
+#endif
+#if UNITY_STANDALONE_OSX
+      syphonServer.enabled = isOn;
+#endif
     }
   }
 }
